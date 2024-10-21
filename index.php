6224421ea8fe96bf014967d32cf147cff08a3593
Yassir Benjima <Yassirbenjima18@gmail.com>
2024-10-21 19:15:21 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Personal Portfolio Website | By Yassir Benjima</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.6.0/css/all.min.css" integrity="sha512-Kc323vGBEqzTmouAECnVceyQqyqdsSiqLQISBL29aUW4U/M7pSPA/gEUZQqv1cwx4OnYxTxve5UMg5GT6L4JJg==" crossorigin="anonymous" referrerpolicy="no-referrer" />
+    <link rel="stylesheet" href="css/style.css" />
+</head>
+
+<body>
+    <header class="header">
+        <div id="menu-btn" class="fas fa-bars"></div>
+        <a href="#home" class="logo">Portfolio</a>
+        <nav class="navbar">
+            <a href="#Home" class="active">Home</a>
+            <a href="#About">About</a>
+            <a href="#Services">Services</a>
+            <a href="#Portfolio">Portfolio</a>
+            <a href="#Cantact">Cantact</a>
+        </nav>
+        <div class="follow">
+            <a href="#" class="fab fa-facebook-f"></a>
+            <a href="#" class="fab fa-twitter"></a>
+            <a href="#" class="fab fa-instagram"></a>
+            <a href="#" class="fab fa-linkedin"></a>
+            <a href="#" class="fab fa-github"></a>
+
+        </div>
+    </header>
+    <section class="home" id="home">
+        <div class="image">
+            <img src="images/moi.jpg" alt="" />
+        </div>
+        <div class="content">
+            <h3>Hi, I'am Yassir Benjima</h3>
+            <span>Computer Engineering and Networking</span>
+            <p>
+                My name is Yassir BENJIMA. I am a third-year student in Computer Engineering and Networks at the Moroccan School of Engineering Sciences (EMSI)
+            </p>
+            <a href="#about" class="btn">about me</a>
+        </div>
+    </section>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
